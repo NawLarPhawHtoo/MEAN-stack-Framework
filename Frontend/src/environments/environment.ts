6a644f75e8b389ae9caf63f0104c515ed0d760f0
Ye Thiha <scm.yethiha@gmail.com>
2023-01-16 08:55:10 +0630

    changes databinding in dialog box

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   authApiUrl: "http://localhost:3000/api",
   apiUrl: "http://localhost:3000/api/v1",
-  storageUrl: "http://localhost:3000/api/v1"
+  storageUrl: "http://localhost:3000/"
 };
